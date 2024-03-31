5b3f014eda123fea3fa3dcfb5acc699efe299be1
Noya <qimiaocai011110@gmail.com>
2024-03-31 16:21:56 -0400

    bug fixed

--- Assets/Scripts/IDkeeper.cs
+++ Assets/Scripts/IDkeeper.cs
@@ -6,9 +6,32 @@ public class IDkeeper : MonoBehaviour
 {
     // Start is called before the first frame update
     public int id;
+    public Shop shop;
+    public Inventory backpack;
+    public ShopUI shopUI;
     void Start()
     {
-        
+        backpack = GameObject.Find("Player").GetComponent<Inventory>();
+    }
+
+    public void SetShops(Shop shop, ShopUI shopUI)
+    {
+        //print(shop.gameObject.name + shopUI.gameObject.name);
+        this.shop = shop; 
+        this.shopUI = shopUI;
+    }
+    public void onButtonClickCoin()
+    {
+        shop.purchase_item(shop.m_coin, id, backpack);
+
+        shopUI.updateItemUICoin(id);
+    }
+
+    public void onButtonClickMoney()
+    {
+        shop.purchase_item(shop.m_money, id, backpack);
+
+        shopUI.updateItemUIMoney(id);
     }
 
     // Update is called once per frame
@@ -16,4 +39,5 @@ public class IDkeeper : MonoBehaviour
     {
         
     }
+
 }
