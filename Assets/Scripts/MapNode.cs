37e3c8d1d9beaf83eebe2b01b1174cdfbdacd09b
Noya <qimiaocai011110@gmail.com>
2024-04-04 21:23:45 -0400

    path uploaded

--- Assets/Scripts/MapNode.cs
+++ Assets/Scripts/MapNode.cs
@@ -27,20 +27,21 @@ public class MapNode : MonoBehaviour
         lr.startWidth = 0.1f;        lr.endWidth = 0.1f;
         lr.startColor = Color.white; lr.endColor = Color.white;
         lr.positionCount = 3;
+        lr.material = Resources.Load<Material>("Path");
         int i = 0;
         if (right != null)
         {
-            lr.SetPosition(0, transform.position);
-            lr.SetPosition(1, right.transform.position);
-            lr.SetPosition(2, transform.position);
+            lr.SetPosition(0, transform.position + new Vector3(0.5f, 0, -0.0f));
+            lr.SetPosition(1, right.transform.position + new Vector3(-0.5f, 0, -0.0f));
+            lr.SetPosition(2, transform.position + new Vector3(0.5f, 0, -0.0f));
             i = 3;
         }
         if(up != null)
         {
             lr.positionCount = i + 3;
-            lr.SetPosition(i, transform.position);
-            lr.SetPosition(i+1, up.transform.position);
-            lr.SetPosition(i+2, transform.position);
+            lr.SetPosition(i, transform.position + new Vector3(0, 0.5f, -0.0f));
+            lr.SetPosition(i+1, up.transform.position + new Vector3(0, -0.5f, -0.0f));
+            lr.SetPosition(i+2, transform.position + new Vector3(0, 0.5f, -0.0f));
         }
         
     }
